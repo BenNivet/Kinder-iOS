7896f84f4f8234cd2a33e4290c98b51d7e6e7ce1
MacB_can <cante.benjamin@gmail.com>
2015-03-15 13:57:23 +0100

    Initial Commit

---
+++ Kinder-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kinder-iOS
+//
+//  Created by Mac portable b_can on 15/03/2015.
+//  Copyright (c) 2015 Niv-Babs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
